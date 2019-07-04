293600194e0463e470919c03a0bb01b149ab7523
codebug8 <livelwh@outlook.com>
2019-07-04 04:42:10 -0400

    Update digitpic shell script
    Signed-off-by: codebug8 <livelwh@outlook.com>

--- board/100ask/ti-am335x/rootfs-overlay/etc/arduino-otg.sh
+++ board/100ask/ti-am335x/rootfs-overlay/etc/arduino-otg.sh
@@ -1,25 +1,4 @@
 #!/bin/sh
-
-#Get the way to start the device 
-LINE=$(cat /proc/cmdline )
-VAR=$(echo ${LINE:22:10} | awk '{print $1}')
-# Get the process pid by name
-PIDS=$(ps -A |grep "am335x_app"| awk '{print $1}')
-UBI='root=ubi0'
-
-if [ $VAR = $UBI ]; then
-        dd  if=/dev/zero  of=/root/disk.img  bs=1M  count=10 > /dev/null
-        mkdosfs  /root/disk.img
-        mount -t vfat -o sync  /root/disk.img  /arduino
-        modprobe  g_mass_storage file=/root/disk.img  removable=1
-else
-	mkdosfs  /dev/mmcblk0p3
-	
-	mount -t vfat   /dev/mmcblk0p3  /arduino
-	modprobe  g_mass_storage file=/dev/mmcblk0p3   removable=1
-	
-fi
-
 mkdir -p /arduino/arduino-100ask
 MYFILE=/arduino/arduino-100ask/am335x_app
 while :
@@ -35,3 +14,4 @@ then
 fi
         sleep 3
 done
+
