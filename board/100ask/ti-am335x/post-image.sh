0e0f52feb79e831ad3cd82eceb49626ac774945e
codebug8 <livelwh@outlook.com>
2019-07-09 23:17:16 -0400

    Update buildroot compile successful printout
    Signed-off-by: codebug8 <livelwh@outlook.com>

--- board/100ask/ti-am335x/post-image.sh
+++ board/100ask/ti-am335x/post-image.sh
@@ -17,4 +17,38 @@ genimage \
     --tmppath "${GENIMAGE_TMP}" \
     --inputpath "${BINARIES_DIR}" \
     --outputpath "${BINARIES_DIR}" \
-    --config "${GENIMAGE_CFG}"
+    --config "${GENIMAGE_CFG}" > /dev/null
+
+mkdir -p  output/images/nfs_rootfs
+mkdir -p  output/images/Nandflash_system
+mv output/images/rootfs.tar.gz  	output/images/nfs_rootfs
+mv output/images/rootfs.ubi   		output/images/Nandflash_system
+cp output/images/MLO  		  		output/images/Nandflash_system
+cp output/images/u-boot.img   		output/images/Nandflash_system
+cp output/images/uEnv.txt     		output/images/Nandflash_system
+cp output/images/zImage				output/images/Nandflash_system
+cp output/images/100ask-am335x.dtb  output/images/Nandflash_system
+
+echo "-------------------------------------------------------"
+
+echo " Uboot  image:  		 `ls -lh  output/images/MLO`"
+echo " Uboot  image:  		 `ls -lh  output/images/u-boot.img`"
+echo " Uboot  image:  		 `ls -lh  output/images/uEnv.txt` "
+
+echo " Linux kernel   image:  	 `ls -lh output/images/zImage`"
+echo " Linux kernel   image:  	 `ls -lh output/images/100ask-am335x.dtb`  \033[0m"
+
+echo " nfs   rootfs   files:  	 `ls -lh output/images/nfs_rootfs/*`"
+
+echo "                                                               "
+echo " SDcard system  image:     `ls -lh  output/images/sdcard.img ` "
+
+echo " Nandflash system image:   `ls -lh  output/images/Nandflash_system/MLO`"
+echo " Nandflash system image:   `ls -lh  output/images/Nandflash_system/u-boot.img`"
+echo " Nandflash system image:   `ls -lh  output/images/Nandflash_system/uEnv.txt  `"
+echo " Nandflash system image:   `ls -lh  output/images/Nandflash_system/zImage`"
+echo " Nandflash system image:   `ls -lh  output/images/Nandflash_system/100ask-am335x.dtb`"
+echo " Nandflash system image:   `ls -lh  output/images/Nandflash_system/rootfs.ubi`"
+
+
+echo "\033[32m Successful system construction!   \033[0m"
